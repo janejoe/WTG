9346a1f021a86262450afe8f772ac27e8013f5a0
janejoe <novozhilova.vm@gmail.com>
2016-01-08 17:51:26 +0300

    new

--- app/src/main/java/jj/wtg/MainActivity.java
+++ app/src/main/java/jj/wtg/MainActivity.java
@@ -3,6 +3,7 @@ package jj.wtg;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,14 +12,30 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] scope = new String[]{VKScope.WALL, VKScope.AUDIO};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        //System.out.println(Arrays.asList(fingerprints));
+
+        VKSdk.login(this, scope);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         if (toolbar != null) {
@@ -31,6 +48,25 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction.replace(R.id.frameContent, fragment).addToBackStack( "tag" ).commit();
         
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+                Toast.makeText(getApplicationContext(),"Good!", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+                Toast.makeText(getApplicationContext(),"Error!", Toast.LENGTH_SHORT).show();
+
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
